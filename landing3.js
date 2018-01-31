1f3fc552908f66852f0effdb89cd2c700057fd0c
Rebecca Schrader <rmschrader1994@gmail.com>
2018-01-31 12:04:02 +0000

    landing page styled and game2 and 3 created

--- landing3.js
+++ landing3.js
@@ -1,9 +1,9 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
-"There are only 10 types of people in the world:", 
-"Those who understand binary, and those who don't"
+"Potential storms ahead,",
+"Proceed with caution"
 );
-var iSpeed = 100; // time delay of print out
+var iSpeed = 130; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
