53fc05a0d4f9ca0a4006007cd0630616c4fc844f
Rebecca Schrader <rmschrader1994@gmail.com>
2018-02-02 17:02:41 +0000

    fonts changed

---
+++ quiz3.js
@@ -0,0 +1,15 @@
+
+function check0(){
+    alert("You finished the Sins of the Father quiz, you should try the other quizzes!")
+}
+
+function check1(){
+    alert("You finished quiz the Beat Within quiz, you should try the other quizzes!")
+}
+
+function check2(){
+    alert("You finished quiz the Blood of the Sacred, Blood of the Damned, you should try the other quizzes!")
+}
+	
+	
+	
